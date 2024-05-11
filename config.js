d641a54c89463beb05023e92d591fe7cc7c356c3
pvpcool <151377608+pvpcool@users.noreply.github.com>
2024-05-11 11:23:24 +0000

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ import axios from 'axios'
 global.owner = [
   ['94767939688', 'Dinuwa Official²⁰²³', true],
   ['94767939688', 'Dinuwa Official²⁰²³', true], 
-  [''] 
+  ['2250565651420','Aaron Lewis', true] 
 ] //Number of owners
 
 global.mods = ['94767939688','94767939688'] 
